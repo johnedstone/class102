1ec7216d8cd91a2dca164ea261fa521fa4d578b0
johnedstone <johnedstone@gmail.com>
2017-10-10 18:02:05 -0400

    gettting ready to try openshift template

--- chap05_adding_json_fields/restapi_project/typical_aws_keys.sh
+++ chap05_adding_json_fields/restapi_project/typical_aws_keys.sh
@@ -23,8 +23,8 @@ export DATABASE_USER=db_user
 export DATABASE_PASSWORD=db_password
 
 ## Optional
-# unset DEBUG
-# unset SECRET_KEY
+# unset DJANGO_DEBUG
+# unset DJANGO_SECRET_KEY
 # unset ALLOWED_HOSTS
 # unset PROJECT_LOGGING_LEVEL
 # unset SUCCESS_MSG_NEW_BUCKET
@@ -35,8 +35,8 @@ export DATABASE_PASSWORD=db_password
 # unset AWS_LOCATION_CONSTRAINT_CHOICES
 # unset AWS_BUCKET_NAME_COMPLIANT_MSG
 
-# export DEBUG=
-# export SECRET_KEY=
+# export DJANGO_DEBUG=
+# export DJANGO_SECRET_KEY=
 # export ALLOWED_HOSTS=
 # export PROJECT_LOGGING_LEVEL= # WARNING|INFO
 # export SUCCESS_MSG_NEW_BUCKET=
